0df003bcceebb189fbe89602fe4ec87bac575e9b
jauhara-nk <jauharakitty@gmail.com>
2024-06-12 15:27:52 +0300

    rating and choose your doctor page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Docprofile from './Components/Docprofile'
+import CheckboxList from './Components/Feedback';
+import Submitbutton from './Components/SubmitButton';
+import Review from './Components/Review';
+
+import DoctorSelection from './Components/Choosedoctor/Selectdoc';
+
+import './Docprofile.css'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
+  
+     
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+     
+    
+       <Docprofile />
+      <CheckboxList />
+      <Review />
+     <Submitbutton /> 
+
+  <DoctorSelection />  
+
+
+     </div>
   )
 }
 
-export default App
+
+   export default App
