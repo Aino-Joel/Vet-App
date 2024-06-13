b47b29e6d8acc4e7139b2bd91e8f6fd2e8155f57
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-06-13 14:47:47 +0300

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Header from './components/Header'
+import Footers from './components/Footers'
+import Farmvisit from './pages/Farmvisit'
+import Urgentcare from'./pages/Urgentcare'
+import Vetclinic from './pages/Vetclinic'
+import Videovisit from './pages/Videovisit'
+import Appointment from './pages/Appointment'
+
 
 function App() {
-  
-
   return (
-   <div>
-    <h1>hello</h1>
-   </div>
+<BrowserRouter>
+<Header/>
+
+<Routes>
+  <Route path ="/" element = {<Home/>}/>
+  <Route path ="/about" element = {<About/>}/>
+  <Route path ="/projects" element = {<Projects/>}/>
+  <Route path ="/signin" element = {<Signin/>}/>
+  <Route path ="/signup" element = {<Signup/>}/>
+  <Route path ="/farmvisit" element = {<Farmvisit/>}/>
+  <Route path ="/urgentcare" element = {<Urgentcare/>}/>
+  <Route path ="/vetclinic" element = {<Vetclinic/>}/>
+  <Route path ="/videovisit" element = {<Videovisit/>}/>
+  <Route path="/appointment" element = {<Appointment/>}/>
+  
+  
+  
+  
+  
+  
+  
+</Routes>
+<Footers/>
+</BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Farmvisit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Farmvisit() {
+  return (
+    <div>
+    <h1>farmvisit</h1>
+
+    </div>
+  )
+}
+
+export default Farmvisit
