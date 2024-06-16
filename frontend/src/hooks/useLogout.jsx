c459a0e9c9b65fafa23dc422e74668d01bf409d4
AinoJoel <ainojoe77@gmail.com>
2024-06-16 15:28:35 +0300

    added context

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,10 +1,7 @@
 import { useAuthContext } from "./useAuthContext"
-// import { useHostelsContext } from "./useHostelsContext"
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
-    //clear global hostel state
-    // const { dispatch: hostelsDispatch } = useHostelsContext()
 
     const logout = () => {
         //remove user from storage
@@ -12,7 +9,6 @@ export const useLogout = () => {
 
         //dispatch logout action
         dispatch({type: 'LOGOUT'})
-        // hostelsDispatch({type: 'SET_HOSTELS', payload: null})
     }
 
     return {logout}
